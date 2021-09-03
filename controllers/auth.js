cea54c4d9c6484767adfc3d0139c9c5422a9858b
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-03 13:39:12 -0400

    importing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
+const crypto = require('crypto');
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat');
+
 const login = (req, res) => {
     try {
+        const { fullName, username, password, phoneNumber } = req.body;
 
+        const userId = crypto;
     } catch(error){
         console.log(error);
 
