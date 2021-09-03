7832c558ad2e0d69ce9ae799447fc478eb30902e
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-03 13:35:44 -0400

    setting up try and catch for the requests

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,21 @@
-const login = () => {
+const login = (req, res) => {
+    try {
 
+    } catch(error){
+        console.log(error);
+
+        res.status(500).json({ message: error})
+    }
 };
 
-const signup = () => {
+const signup = (req, res) => {
+    try {
 
+    } catch(error){
+        console.log(error);
+
+        res.status(500).json({ message: error})
+    }
 };
 
 module.exports = {
