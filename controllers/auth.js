e1eb0a426b7cbaf8487147ac49aa5892d436928f
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-03 13:43:00 -0400

    stream secrets

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,10 @@ const { connect } = require('getstream');
 const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat');
 
+const api_key = process.env.STREAM_API_KEY;
+const api_secret = process.env.STREAM_API_SECRET;
+const app_id = process.env.STREAM_API_ID;
+
 const login = (req, res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
