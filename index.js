07a0968c03f09555071cb5df1f5bb95b1f731de3
johnnie71 <johnnie.agonz71@gmail.com>
2021-09-21 20:19:19 -0400

    environment variables

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 // enabling us to call the environmental variables inside our application
 require('dotenv').config();
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+// Twilio account allowing us to make the request in order to get SMS notifications
+const client = require('twilio')(accountSid, authToken);
+
 // allowing cross origin requests
 app.use(cors());
 // allowing to pass json payloads from the frontend to the backend
